7cb94ac3716fec008e8d8300cd2ece446d0ed12f
Kanishk Tanotra <59711945+k-tanotra@users.noreply.github.com>
2025-02-07 15:04:46 -0700

    fix for last errors

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,9 +1,6 @@
 "use client"
 import React from 'react';
 import Contact from '@/components/Contact/Contact';
-import { Button } from "@/components/ui/button";
-import { ArrowBigRight} from 'lucide-react';
-import Link from 'next/link';
 export default function Page() {
   return (
     <div>
